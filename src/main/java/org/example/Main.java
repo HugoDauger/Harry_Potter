993996015e277a75cfccc42034571345c68b1b78
hdaug <hdauger2@gmail.com>
2023-04-03 09:46:54 +0200

    Pets

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,13 @@
 package org.example;
-
-import com.isep.character.House;
-import com.isep.character.Wand;
+import com.isep.Pet;
+import com.isep.character.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         House.house();
         Wand.wand();
+        Pet ChoicePet = Pet.selectPet();
+        System.out.println("your pet is a " + ChoicePet + " ");
     }
 }
